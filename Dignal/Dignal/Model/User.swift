02cb727a9990bcf089b9932ad88499acf0a69186
정정욱 <jeonguk29@naver.com>
2025-04-02 12:19:09 +0900

    [Feat] : User Model 구현

---
+++ Dignal/Dignal/Model/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Dignal
+//
+//  Created by 정정욱 on 3/30/25.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var studentID: String
+    var gender: String
+    var isDKUStudent: Bool // 단국대
+    var phoneNumber: String
+    var mbti: String
+    var age: String
+    var height: String
+    var lookalike: String
+    var instagramID: String
+}
